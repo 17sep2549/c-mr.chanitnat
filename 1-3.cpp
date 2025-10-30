be3bc2543d1ca057e56c08c6b1a512276400f0cc
17sep2549 <chatchai0100@gmail.com>
2025-10-30 11:45:41 +0700

    Implement basic calculator functionality

---
+++ 1-3.cpp
@@ -0,0 +1,47 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+
+  char op;
+  float num1, num2 , result;
+
+  cout << "Enter operator: +, -, *, /: ";
+  cin >> op;
+
+  cout << "Enter First operands: ";
+  cin >> num1;
+  cout << "Enter second operands: ";
+  cin >> num2;
+  
+ 
+  switch(op) {
+
+    case '+':
+    result = num1 + num2;
+        cout << num1 << " + " << num2 << " = " << num1 + num2;
+        break;
+
+    case '-':
+    result = num1 - num2;
+        cout << num1 << " - " << num2 << " = " << num1 - num2;
+        break;
+
+    case '*':
+    result = num1 * num2;
+        cout << num1 << " * " << num2 << " = " << num1 * num2;
+        break;
+
+    case '/':
+    result = num1 / num2;
+        cout << num1 << " / " << num2 << " = " << num1 / num2;
+        break;
+
+    default:
+      // If the operator is other than +, -, * or /, error message is shown
+      cout << "Error! operator is not correct";
+      break;
+  }
+
+  return 0;
+}
